43fbabfe8aed21be058e556f98ccf8c8c6a48f4c
Clément Hue <clementhue@hotmail.fr>
2025-06-03 11:14:02 +0200

    Input label turn red when error

--- frontend/app/components/Input/Input.tsx
+++ frontend/app/components/Input/Input.tsx
@@ -1,5 +1,6 @@
 import React, {useId} from "react"
 import {useField} from "formik";
+import clsx from "clsx";
 
 const Input = React.forwardRef<HTMLInputElement, Props>(({label, required, ...props}, ref) => {
     const id = useId()
@@ -7,10 +8,11 @@ const Input = React.forwardRef<HTMLInputElement, Props>(({label, required, ...pr
     const [field, meta] = useField(props)
    return (
        <div className="flex flex-col gap-1">
-           {label && <label htmlFor={id} >{label}{required && <span className="text-red-600"> *</span>}</label>}
-           <input aria-errormessage={meta.error ? errorId : undefined} placeholder={label} className="
-            border-2 outline-primary-600 border-primary-500 p-1 rounded-md  focus:border-primary-600 focus:outline-1
-           " required={required} id={id} ref={ref} {...field} {...props}/>
+           {label && <label htmlFor={id} className={clsx({
+               "text-red-500": meta.touched && !!meta.error,
+           })} >{label}{required && <span className="text-red-600"> *</span>}</label>}
+           <input aria-errormessage={meta.error ? errorId : undefined} placeholder={label} className={clsx("border-2 outline-primary-600 border-primary-500 p-1 rounded-md  focus:border-primary-600 focus:outline-1 ")}
+               required={required} id={id} ref={ref} {...field} {...props}/>
            <div id={errorId} className="text-sm min-h-5 text-red-500">{meta.touched && meta.error}</div>
        </div>
    )
